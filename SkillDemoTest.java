b8c702922c7f9c6211d8f461065e77f46d1f8777
Kryptix3k <102953250+Kryptix3k@users.noreply.github.com>
2022-05-01 22:38:40 -0700

    Fixed

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,12 +1,12 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class SkillDemoTest {
+public class SkillDemoTest extends javafx.application.Application {
 
 
 @Test
 public void testSubtraction() {
-assertEquals(2,SkillDemo.subtract(5,4) );
+assertEquals(2,SkillDemo.subtract(5,4));
 }
 }
 
